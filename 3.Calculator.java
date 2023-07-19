bb4bdc18c99e4ae5d28994a64b54d498d8351dba
NIRAJ AGARWAL <109128838+Niraj-Agarwal@users.noreply.github.com>
2023-07-19 23:05:44 +0530

    Simple Calculator.java

---
+++ 3.Calculator.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Calculator{
+    public static void main(String args[]) {
+        int op1, op2, res;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter ther first operand");
+        op1 = sc.nextInt();
+        System.out.println("Enter the second operand");
+        op2 = sc.nextInt();
+        char operator;
+        System.out.print("Enter the operator (+,-,*,/,%): ");
+        System.out.println();
+        operator = sc.next().charAt(0);
+        switch (operator) {
+            case '+':
+                res = op1 + op2;
+                System.out.println("The addition is " + res);
+                break;
+            case '-':
+                res = op1 - op2;
+                System.out.println("The subtraction is " + res);
+                break;
+            case '*':
+                res = op1 * op2;
+                System.out.println("The multiplication is " + res);
+                break;
+            case '/':
+                res = op1 / op2;
+                System.out.println("The division is " + res);
+                break;
+            case '%':
+                res = op1 % op2;
+                System.out.println("The division is " + res);
+                break;
+            default:
+                System.out.println("Invalid Input");
+                break;
+        }
+    }
+}
